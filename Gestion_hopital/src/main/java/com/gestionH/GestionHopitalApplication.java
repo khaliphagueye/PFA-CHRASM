e5da449d0ba7623160086fc18792b30c8d7825a8
khali <khaliphag9@gmail.com>
2021-07-03 19:23:02 +0100

    //

--- Gestion_hopital/src/main/java/com/gestionH/GestionHopitalApplication.java
+++ Gestion_hopital/src/main/java/com/gestionH/GestionHopitalApplication.java
@@ -1,19 +1,26 @@
 package com.gestionH;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gestionH.repo.PatientRepo;
 @SpringBootApplication
-public class GestionHopitalApplication  {
+public class GestionHopitalApplication implements CommandLineRunner {
 	
 	@Autowired
 	PatientRepo pr;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GestionHopitalApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
